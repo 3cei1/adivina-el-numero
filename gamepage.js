b522fa0086f875de32c7deb528e26febb44a2f00
3cei1 <138079346+3cei1@users.noreply.github.com>
2024-02-19 20:59:49 -0600

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,16 @@
+var y
+  
+// contar el número de intentos
+// crear para el intento correcto
+
+  
+// función para el número adivinado por el usuario     
+var x = document.getElementById("guessField").value
+
+if (x == y) { 
+    alert("¡¡¡Adivinaste!!!" + playername + guess + "intento");
+    guess = 1
+} else if (x > y) {
+    alert()
+    
+}
